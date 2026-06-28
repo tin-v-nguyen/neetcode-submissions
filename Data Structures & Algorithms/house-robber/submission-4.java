d00fb8134ad5b13e318c8c6539673efeba78c3f6
Tin Van Nguyen <104019662+tin-v-nguyen@users.noreply.github.com>
2026-06-28 02:12:59 -0400

    Add: house-robber - submission-4

---
+++ Data Structures & Algorithms/house-robber/submission-4.java
@@ -0,0 +1,13 @@
+class Solution {
+    private int[] memo;
+    public int rob(int[] nums) {
+        memo = new int[nums.length];
+        return dfs(nums, 0);
+    }
+
+    private int dfs(int[] nums, int i) {
+        if (i >= nums.length) return 0;
+        if (memo[i] != 0) return memo[i];
+        return memo[i] = Math.max(dfs(nums, i + 1), nums[i] + dfs(nums, i + 2));
+    }
+}
